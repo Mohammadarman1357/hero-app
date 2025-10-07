ba4380e04a9326486072496a8ea6bf102592a180
Arman <mohammadjahedulislamarman@gmail.com>
2025-10-08 00:45:57 +0600

    added header and footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import logoImg from '../../assets/logo.png';
+import { Facebook, Linkedin, Twitter } from 'lucide-react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>hello footer</h1>
+        <div className='bg-[#001931] py-8 px-15 inter-font'>
+            <div className='flex justify-between items-center'>
+                <span className='flex items-center'>
+                    <img className='w-10 h-10' src={logoImg} alt="" />
+                    <h1 className='text-white text-xl font-bold ml-2'>HERO.IO</h1>
+                </span>
+
+                <span className='text-white'>
+                    <h1 className='font-medium text-xl'>Social Links</h1>
+                    <span className='flex gap-4 '>
+                        <Twitter />
+                        <Linkedin />
+                        <Facebook />
+                    </span>
+                </span>
+            </div>
+            <hr className='border-[#E5E7EB] my-4'/>
+            <p className='text-center text-[#FAFAFA]'>Copyright © 2025 - All right reserved</p>
         </div>
     );
 };
